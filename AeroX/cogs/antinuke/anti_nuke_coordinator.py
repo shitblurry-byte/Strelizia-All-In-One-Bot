cd1b9ee5bfaba511669fdbf1c2a61c8e8a9d21a2
shitblurry-byte <shitblurry@gmail.com>
2025-12-05 17:25:36 -0500

    Delete AeroX directory

--- AeroX/cogs/antinuke/anti_nuke_coordinator.py
+++
@@ -1,9 +0,0 @@
-
-
-pass 
-"""
-: ! Aegis !
-    + Discord: root.exe
-    + Community: https://discord.gg/meet (AeroX Development )
-    + for any queries reach out Community or DM me.
-"""
--- AeroX/logs/top-gg/server.py
+++
@@ -1,8 +0,0 @@
-from app import app 
-
-"""
-: ! Aegis !
-    + Discord: root.exe
-    + Community: https://discord.gg/meet (AeroX Development )
-    + for any queries reach out Community or DM me.
-"""
