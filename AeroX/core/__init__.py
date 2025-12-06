5da2de1643bc5d2ca4663c9fd34212d765db7e9b
shitblurry-byte <shitblurry@gmail.com>
2025-12-06 00:54:36 -0500

    Delete AeroX directory

--- AeroX/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Strelizia import Strelizia 
-from .Context import Context 
-from .Cog import Cog 
-"""
-: ! Aegis !
-    + Discord: root.exe
-    + Community: https://discord.gg/meet (AeroX Development )
-    + for any queries reach out Community or DM me.
-"""
